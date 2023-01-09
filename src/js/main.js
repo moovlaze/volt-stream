67a17ddcfff372cfb3cdc956d4713b24bacb1430
Max <thewildstar.m@gmail.com>
2023-01-10 00:45:59 +0300

    added section reviews

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,53 @@ const openBurger = () => {
 	});
 };
 
+const slider = (selector) => {
+	const swiper = new Swiper(selector, {
+		slidesPerView: "auto",
+		spaceBetween: 30,
+		navigation: {
+			nextEl: ".arrows__rigth",
+			prevEl: ".arrows__left",
+		},
+		disabledClass: ".arrows__left",
+	});
+};
+
+const galleryOpenAndClose = () => {
+	const body = document.querySelector("body");
+	const galleryItem = document.querySelectorAll(".gallery-wrapper img");
+
+	galleryItem.forEach((item) => {
+		item.addEventListener("click", () => {
+			let imgSrc = item.getAttribute("src");
+			let imgAlt = item.getAttribute("alt");
+
+			body.insertAdjacentHTML(
+				"beforeend",
+				`
+			<div class="gallery-box">
+				<img src="${imgSrc}" alt="${imgAlt}">
+			</div>
+			`
+			);
+
+			closeGallery();
+		});
+	});
+
+	const closeGallery = () => {
+		const galleryBody = document.querySelector(".gallery-box");
+
+		galleryBody.addEventListener("click", (e) => {
+			let newImg = galleryBody.querySelector("img");
+
+			if (e.target !== newImg) {
+				galleryBody.remove(body);
+			}
+		});
+	};
+};
+
 openBurger();
+slider(".reviews__slider");
+galleryOpenAndClose();
